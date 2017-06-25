80edca5a890c208b5f50d48c260a8a7091b6d10f
vlapitsky <vasil.deville@gmail.com>
2017-06-25 10:04:19 -0400

    My second f***ing git commit

---
+++ blog/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey('auth.User')
+    title = models.CharField(max_length=200)
+    text = models.CharField(max_length=2000)
+    date_created = models.DateTimeField(default=timezone.now)
+    date_published = models.DateTimeField(blank=True, null=True)
+
+    def publish(self):
+        self.date_published = timezone.now()
+
+    def __str__(self):
+        return self.title
