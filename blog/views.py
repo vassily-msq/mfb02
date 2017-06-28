a2422872371896250b562b830787d6657b6d1671
vlapitsky <vasil.deville@gmail.com>
2017-06-28 16:46:10 -0400

    major things

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from django.utils import timezone as tz
 
@@ -7,3 +7,7 @@ def post_list(request):
     published = Post.objects.filter(date_published__lte = tz.now()).order_by('date_published')
     r=render(request, 'blog/post_list.html', {'posts': published})
     return r
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
